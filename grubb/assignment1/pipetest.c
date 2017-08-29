4b2264211336713971b8bc0218ce2c5e7da9e623
Elijah Grubb <u0894728@utah.edu>
2017-08-29 03:31:11 -0600

    modified for xv6 and added dif sizes of writes and reads

--- grubb/assignment1/pipetest.c
+++ grubb/assignment1/pipetest.c
@@ -1,51 +1,80 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <unistd.h>
+#include "types.h"
+#include "stat.h"
+#include "user.h"
 
-uint32_t xorshift32(uint32_t state) {
-  uint32_t x = state;
-  x ^= x << 13;
-  x ^= x >> 17;
-  x ^= x << 5;
-  state = x;
-  return x;
-}
-
-void testBasicPipeThroughPut() {
-  int fd[2];
-  uint32_t seed = 313;
-  int i, j;
-  pipe(fd);
-  int BUFFER_LEN = 4;
-  char buffer[BUFFER_LEN];
-  
-  if (fork() == 0) {
-    close(fd[1]);
-    for (i = 0; i < 1000; i++) {
-      for (j = 0; j < BUFFER_LEN; j++) {
-        seed = xorshift32(seed);
-        buffer[j] = (unsigned char) seed & 0xfff;
-      }
-      char temp[BUFFER_LEN];
-      int nbytes = read(fd[0], temp, sizeof(temp));
-      for (j = 0; j < BUFFER_LEN; j++) {
-        if ((unsigned char) temp[j] != (unsigned char) buffer[j]) {
-          printf("Correct: %x, Actual: %x\n", buffer[j], temp[j]);
-        }
-      } 
-    }
-  } else {
-    close(fd[0]);
-    for (i = 0; i < 1000; i++) {
-      for (j = 0; j < BUFFER_LEN; j++) {
-        seed = xorshift32(seed);
-        buffer[j] = (unsigned char) seed & 0xfff;
-      }
-      write(fd[1], buffer, sizeof(buffer));
-    }
-  }
+// borrowed and modified from usertests.c
+unsigned long
+rand(unsigned long randstate)
+{
+  randstate = randstate * 1664525 + 1013904223;
+  return randstate;
 }
 
 int main() {
-  testBasicPipeThroughPut();
+  int fd[2];
+  pipe(fd); 
+
+  unsigned long streamstate = 1;
+ 
+  if (fork() == 0) {
+    //child
+    close(fd[1]);
+   
+    unsigned long procstate = 2;
+ 
+    int i,j;
+    for (i = 0; i < 1000000; ) {
+      procstate = rand(procstate);
+      int BUF_SIZE = (int) procstate % 1000;
+      if (BUF_SIZE < 0) {
+        BUF_SIZE = BUF_SIZE * -1;
+      }
+      char buf[BUF_SIZE];
+      int nbytes = read(fd[0], buf, sizeof(buf));
+      for (j = 0; j < nbytes; j++) {
+        streamstate = rand(streamstate);
+        char correctChar = (unsigned char) streamstate & 0xfff;
+        if (buf[j] != correctChar) {
+          printf(1, "Correct: %x, Actual: %x\n", correctChar, buf[j]);
+        }
+      }
+      i = i + nbytes;
+    }
+  
+  } else {
+    //parent
+    close(fd[0]);
+
+    unsigned long procstate = 3;
+
+    int i,j;
+    for (i = 0; i < 1000000; ) { 
+      procstate = rand(procstate);
+      int BUF_SIZE = (int) procstate % 1000;
+      if (BUF_SIZE < 0) {
+        BUF_SIZE = BUF_SIZE * -1;
+      }
+      char buf[BUF_SIZE];
+      for (j = 0; j < BUF_SIZE; j++) {
+        streamstate = rand(streamstate);
+        char correctChar = (unsigned char) streamstate & 0xfff;
+        buf[j] = correctChar;
+      }
+      int nbytes = write(fd[1], buf, sizeof(buf));
+      i = i + nbytes;
+      // handle short writes
+      while (nbytes < BUF_SIZE) {
+        BUF_SIZE = BUF_SIZE - nbytes;
+        char temp[BUF_SIZE];
+        for (j = 0; j < BUF_SIZE; j++) {
+          temp[j] = buf[j + nbytes];
+        }
+        nbytes = write(fd[1], temp, sizeof(temp));
+        i = i + nbytes;
+      }
+    }
+    wait();
+  }
+
+  exit();
 }
