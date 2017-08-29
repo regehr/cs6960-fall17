b104d5fef24baf4aba4bdca2d8ca14729e1c679c
John Fresco <john.fresco@utah.edu>
2017-08-29 13:30:10 -0600

    Add simple test with random read writes

--- fresco/assignment1/pipetest.c
+++ fresco/assignment1/pipetest.c
@@ -1,23 +1,112 @@
-#include <inttypes.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <unistd.h>
+#include "types.h"
+#include "user.h"
 
-/* Xorshift cause all the cool people use that nowadays,
+#define FAILED 1
+
+#define STDOUT 1
+#define STDERR 2
+
+/* Xorshift 'cause all the cool people use that nowadays,
  * and we're not doing crypto. */
-static uint32_t random() {
-    static uint32_t state = 0x1337;
+static uint random() {
+    static uint state = 0x1337;
     state ^= state << 13;
     state ^= state >> 17;
     state ^= state << 5;
     return state;
 }
 
+/* A second RNG with a different seed */
+static uint randomC() {
+    static uint state = 0xBEEF;
+    state ^= state << 13;
+    state ^= state >> 17;
+    state ^= state << 5;
+    return state;
+}
+
+int pipeTest(char *stream);
+
 int main(int argc, char **argv) {
-    int fds[2];
-    
-    while(1) {
-        printf("%08X\n", random()); 
-        sleep(1);
+    /* Initialize random stream */
+    char stream[1024];
+
+    for (int i = 0; i < 256; i++) {
+        uint *intOffset = (uint*) (&stream[i*4]);
+        *intOffset = random();
     }
+
+    if (pipeTest(stream)) {
+        printf(STDOUT, "Failed\n");
+    } else {
+        printf(STDOUT, "Success\n");
+    }
+
+    exit();
+}
+
+int pipeTest(char *stream) {
+    int fds[2];
+    int pid;
+
+    /* Error checking */
+    if (pipe(fds) != 0) {
+        printf(STDERR, "Failed to create pipe.\n");
+        exit();
+    }
+
+    /* Parent */
+    if ((pid = fork())) {
+        int bytesLeft = 1024;
+
+        char buf[1024];
+
+        int randRead;
+        while (!(randRead = (random() & 32)));
+
+        while (bytesLeft > 0) {
+            int bytesRead = read(fds[0], &buf[1024-bytesLeft], randRead > bytesLeft ? bytesLeft : randRead);  
+            if (bytesRead == -1) { break; }
+
+            bytesLeft -= bytesRead;
+        }
+
+        close(fds[0]);
+        wait();
+
+        /* Test: Correct number of bytes transfered */
+        if (bytesLeft > 0) {
+                printf(STDOUT, "Failed: The numbers of bytes read did not match\n");
+                printf(STDOUT, "bytesLeft: %d\n", bytesLeft);
+                return FAILED;
+        }
+
+        /* Test: Data was written correctly and read out in order */
+        for (int i = 0; i < 1024; i++) {
+            if (buf[i] != stream[i]) {
+
+                printf(STDOUT, "Failed: Bytes written to buffer did not match the initial stream\n");
+                return FAILED;
+            }
+        }
+    }
+
+    /* Child 
+     * 
+     * The child will end up doing the writing so the parent can handle 
+     * verification without the child having to communicate anything else. */
+    else {
+        int bytesLeft = 1024;
+
+        int randWrite;
+        while (!(randWrite = (randomC() & 32)));
+        while (bytesLeft > 0) {
+            bytesLeft -= write(fds[1], &stream[1024-bytesLeft], randWrite > bytesLeft ? bytesLeft : randWrite);
+        }
+
+        close(fds[1]);
+        exit();
+    }
+
+    return 0;
 }
