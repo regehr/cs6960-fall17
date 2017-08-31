a027ea1b1755da90d06a9da2336010101c7bfc58
John Fresco <john.fresco@utah.edu>
2017-08-31 13:45:50 -0600

    Partially finish writing

--- fresco/assignment1/pipe.c
+++ fresco/assignment1/pipe.c
@@ -1,3 +1,11 @@
+/* I'm gonna annotate this with my thoughts as I write this.
+ * I hope the comments aren't too verbose. */
+
+/* Note: 19:43:24 
+ * I'm running into some bug with my writing code and haven't gotten
+ * to the read part yet. It's probably somewhere in the code handling
+ * when you go over the buffer size. */
+
 #include "types.h"
 #include "defs.h"
 #include "param.h"
@@ -10,9 +18,16 @@
 
 #define PIPESIZE 512
 
+/* How many bytes of freespace in the pipe before considering a short write. */
+#define SHORTW_THRESHOLD 256
+
+/* Stealing your min() from pipespeed.c */
+#define min(x,y) (((x)<(y))?(x):(y)) 
+#define max(x,y) (((x)<(y))?(y):(x))
+
 struct pipe {
-  struct spinlock lock;
   char data[PIPESIZE];
+  struct spinlock lock;
   uint nread;     // number of bytes read
   uint nwrite;    // number of bytes written
   int readopen;   // read fd is still open
@@ -74,27 +89,85 @@ pipeclose(struct pipe *p, int writable)
     release(&p->lock);
 }
 
-//PAGEBREAK: 40
+/* I wish I had time to optimize this, 
+ * but Distributed Systems is due tonight too. :C */
+
+/* Cicular Buffer Notes
+ * --------------------
+ * 
+ * Rereading [1] made me finally grok the magic of using modulo for circulars 
+ * buffers.
+ *
+ * - w ~= r: 
+ *   - w = r:   empty
+ *   - else:     full
+ *   
+ *
+ * [1] https://www.snellman.net/blog/archive/2016-12-13-ring-buffers/
+ *
+ */
 int
 pipewrite(struct pipe *p, char *addr, int n)
 {
-  int i;
+  /* I'm a little torn on the semantics of signaling for pipes.
+   * Does a reader want to know someone tried writing even through they 
+   * wrote zero bytes? I was worried about the reader getting stuck, 
+   * but then I suppose that pipeclose should wake them. I guess I'll quickly
+   * return on zero.
+   * */
+  if (n == 0) return 0;
 
   acquire(&p->lock);
-  for(i = 0; i < n; i++){
-    while(p->nwrite == p->nread + PIPESIZE){  //DOC: pipewrite-full
-      if(p->readopen == 0 || myproc()->killed){
-        release(&p->lock);
-        return -1;
-      }
-      wakeup(&p->nread);
-      sleep(&p->nwrite, &p->lock);  //DOC: pipewrite-sleep
+
+  int freeSpace = PIPESIZE - (p->nwrite - p->nread);
+
+  /* It might cause a lot of switching back and forth between the usercode
+   * and the pipe, so maybe it might be better to block rather than short write
+   * zero bytes.
+   *
+   * TODO: It might be worth trying to tune a threshold for how many bytes are
+   *       needed to consider a shortwrite.
+   *
+   *       ^DID
+   *
+   * TODO: Consider buffer end as another threshold
+   *
+   * - If n is smaller than the threshold, we can check if we have enough space to write that.
+   */
+  while (freeSpace < min(SHORTW_THRESHOLD, n) || !freeSpace) {
+    if (p->readopen == 0 || myproc()->killed) {
+      release(&p->lock);
+      return -1;
     }
-    p->data[p->nwrite++ % PIPESIZE] = addr[i];
+    wakeup(&p->nread);
+    sleep(&p->nwrite, &p->lock);
   }
-  wakeup(&p->nread);  //DOC: pipewrite-wakeup1
+
+  int writeIndex = p->nwrite % PIPESIZE;
+
+  /* Check if we're about to touch the buffer boundary. */
+  int toEnd = PIPESIZE - writeIndex;
+
+  const int firstWrite = min(min(toEnd, freeSpace), n);
+
+  /* First write up to the buffer end */
+  memmove(&p->data[writeIndex], addr, firstWrite);
+
+  p->nwrite += firstWrite;
+  freeSpace -= firstWrite;
+
+  /* Prepare second write in case have more than until the buffer end */
+  int bytesLeft = n - firstWrite;
+
+  const int secondWrite = min(bytesLeft, freeSpace);
+
+  if (secondWrite) {
+    memmove(p->data, &addr[firstWrite], secondWrite);
+  }
+
+  wakeup(&p->nread);
   release(&p->lock);
-  return n;
+  return firstWrite + secondWrite;
 }
 
 int
