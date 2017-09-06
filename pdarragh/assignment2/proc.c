72ed848c71a2c9691e270e503cd49d75f3ea047b
Pierce Darragh <pierce.darragh@gmail.com>
2017-09-05 18:02:28 -0600

    Use queue in scheduler

--- pdarragh/assignment2/proc.c
+++ pdarragh/assignment2/proc.c
@@ -403,28 +403,29 @@ scheduler(void) {
         // Enable interrupts on this processor.
         sti();
 
-        // Loop over process table looking for process to run.
+        // Dequeue a process to run.
         acquire(&ptable.lock);
-        for (p = ptable.proc; p < &ptable.proc[NPROC]; p++) {
-            if (p->state != RUNNABLE)
-                continue;
-
-            // Switch to chosen process.  It is the process's job
-            // to release ptable.lock and then reacquire it
-            // before jumping back to us.
-            c->proc = p;
-            switchuvm(p);
-            p->state = RUNNING;
-
-            swtch(&(c->scheduler), p->context);
-            switchkvm();
-
-            // Process is done running for now.
-            // It should have changed its p->state before coming back.
-            c->proc = 0;
+        p = dequeue(&ptable.ready);
+        if (p == NULL) {
+            // No RUNNABLE process exists.
+            release(&ptable.lock);
+            continue;
         }
-        release(&ptable.lock);
+        // Switch to chosen process.  It is the process's job
+        // to release ptable.lock and then reacquire it
+        // before jumping back to us.
+        c->proc = p;
+        switchuvm(p);
+        p->state = RUNNING;
 
+        swtch(&(c->scheduler), p->context);
+        switchkvm();
+
+        // Process is done running for now.
+        // It should have changed its p->state before coming back.
+        c->proc = 0;
+
+        release(&ptable.lock);
     }
 }
 
