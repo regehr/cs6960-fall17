b7482bb7d4634a69158a9247fd601d61e53e4d3c
Michael McConville <mmcco@mykolab.com>
2017-11-14 03:00:45 -0700

    that's all for tonight, folks (more tomorrow)

--- mmcc/assignment7/ring_buf.c
+++ mmcc/assignment7/ring_buf.c
@@ -1,56 +1,38 @@
-#include <types.h>
-#include <defs.h>
-#include <mmu.h>
+#include "types.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+#include "defs.h"
+#include "x86.h"
 
-static char *pages[9];
+const uint buf_sz = 8 * PGSIZE;
+
+int *ntaken = (int *)0x0000f000;
+char *buf = (char *)0x0000f000 + PGSIZE;
 
 int
-buf_setup(void)
+buf_put(char *data, uint len)
 {
-	int i;
-
-	for (i = 0; i < 9; i++) {
-		if ((pages[i] = kalloc()) == 0)
-			return -1;
-	}
-
-	// unnecessary in any real Unix, but just to be safe
-	for (i = 0; i < PGSIZE; i++)
-		pages[0][i] = 0;
-
-	// number of messages is the first four bytes (init: 0)
-	// ptr to next message is next four bytes (init: NULL)
+	if (len > buf_sz)
+		return -1;
+	
+	// if we're full
+	if (*ntaken == 8)
+		return -1;
 
 	return 0;
 }
 
 int
-buf_put(char *p, int len)
+buf_get(char *data, uint len, uint maxlen)
 {
-	if (len > PGSIZE)
-		return -1
-
-	(int *)pages[0]++;
-}
-
-int
-buf_get(char *p, int len, int maxlen)
-{
-	char *cur_msg = &pages[0][4];
-	int cur_msg_sz = *(int *)cur_msg;
-	char *msg_ptr;
-
-	// there are no messages waiting
-	if (!cur_msg)
+	if (len > buf_sz)
 		return -1;
 
-	if (cur_msg_sz > maxlen)
-		return -2;
-
-	for (i = 0; i < len; i++)
-		p[i] = cur_msg[i+4]
-
-	(int *)pages[0]--;
+	// if we're empty
+	if (*ntaken == 0)
+		return -1;
 
 	return 0;
 }
