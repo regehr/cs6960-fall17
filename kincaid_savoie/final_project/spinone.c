21448dfcff41d0d5be1a3c02dcc2935a47542ca8
Kincaid <kincaidsavoie@yahoo.com>
2017-12-15 19:25:35 -0700

    Final Project Code (#132)
    * Fixed up pipetest.
    
    * Added updated makefile.
    
    * Initial attempt at improvement.
    
    * Fixed issue where random numbers got out of sync on short writes in pipetest.
    
    * Fixed bugs in pipe.c
    
    * Added original pipe.c
    
    * Commented unnecessary printfs.
    
    * Restructured pipe struct. Slight speedup.
    
    * Increased buffer size. Significant speedup.
    
    * Added initial versions of proc.c and proc.h.
    
    * Switched proc.c to use ready queue.
    
    * Switched data buffer to be dynamically allocated.
    
    * Got memcpy working in pipe.c
    
    * Made it so users don't have to call exit manually.
    
    * Added explanation of shell deadlock.
    
    * First pass at implementing mutex.
    
    * Got mutexes compiling into the kernel.
    
    * Created test program for mutex.
    
    * Added assignment 5.
    
    * Stubbed out stuff for implementing shared buffer syscall.
    
    * Removed user space functions from kernel space.
    
    * Added test for shared memory region.
    
    * Impemented shared memory stuff and a test for it.
    
    * Stubbed out user buffer functions.
    
    * Finished ring buffer implementation.
    
    * Added ring buffer test.
    
    * Added final project code.

---
+++ kincaid_savoie/final_project/spinone.c
@@ -0,0 +1,14 @@
+#include "types.h"
+#include "user.h"
+
+int
+main(void)
+{
+  uint counter = 0;
+  while (1) {
+    counter++;
+    if (counter % 10 == 0)
+      printf(1, "%d\n", counter);
+  }
+  exit();
+}
